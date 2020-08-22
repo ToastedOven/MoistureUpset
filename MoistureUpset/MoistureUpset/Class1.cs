90ea665a5cbe17821419e86b321586b281135b58
Zachary Jones <dwelxs2@gmail.com>
2020-08-22 16:05:44 -0400

    I no longer wish to bee alive

--- MoistureUpset/MoistureUpset/Class1.cs
+++ MoistureUpset/MoistureUpset/Class1.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using BepInEx;
+using RoR2;
+using System.Reflection;
+using static R2API.SoundAPI;
 
-namespace MoistureUpset
+namespace Nunchuck
 {
-    public class Class1
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.WetBoys.WetGamers", "We are really wet.", "0.6.9")]
+    public class BigTest : BaseUnityPlugin
     {
+        public void Awake()
+        {
+            using (var bankStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MoistureUpset.ImMoist.bnk"))
+            {
+                var bytes = new byte[bankStream.Length];
+                bankStream.Read(bytes, 0, bytes.Length);
+                SoundBanks.Add(bytes);
+            }
+            On.RoR2.DamageNumberManager.SpawnDamageNumber += (orig, self, amount, position, crit, teamIndex, DamageColorIndex) =>
+            {
+                var mainBody = NetworkUser.readOnlyLocalPlayersList[0].master?.GetBody();
+                orig(self, amount, position, crit, teamIndex, DamageColorIndex);
+                Util.PlaySound("YourMother", mainBody.gameObject);
+                //Util.PlaySound("maybejapanese", mainBody.gameObject);
+                //AkSoundEngine.PostEvent(EntityStates.Commando.CommandoWeapon.FireBarrage.fireBarrageSoundString, mainBody.gameObject);
+                Util.PlaySound(EntityStates.Huntress.HuntressWeapon.ThrowGlaive.attackSoundString, mainBody.gameObject);
+                Chat.AddMessage($"played {EntityStates.Huntress.HuntressWeapon.ThrowGlaive.attackSoundString}");
+            };
+        }
     }
 }
