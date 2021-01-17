9fa2997dc24f49d4fb483c52bcf9e25553729776
Rune <RuneMasterGaming@users.noreply.github.com>
2021-01-16 22:10:24 -0500

    soda time

--- MoistureUpset/MoistureUpset/InteractReplacements/SodaBarrel/RandomizeSoda.cs
+++ MoistureUpset/MoistureUpset/InteractReplacements/SodaBarrel/RandomizeSoda.cs
@@ -8,7 +8,7 @@ namespace MoistureUpset.InteractReplacements.SodaBarrel
 {
     class RandomizeSoda : MonoBehaviour
     {
-        private Texture[] textures = { Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/cokacoon.png") };
+        private Texture[] textures = { Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/cokacoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/drcoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/mtncoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/pepcoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/hicoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/spritecoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/fantacoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/watercoon.png"), Resources.Load<Texture>("@MoistureUpset_InteractReplacements_SodaBarrel_sodaspritz:assets/sodafountain/lioncoon.png") };
         private int currentTexture = 0;
         void Start()
         {
@@ -18,6 +18,12 @@ namespace MoistureUpset.InteractReplacements.SodaBarrel
 
         public Color getSpritzColor()
         {
+            // UnsavedTrash didn't put the pixel color in the second texture, and I don't feel like loading up gimp. so it just copies the coca cola color.
+            if (currentTexture == 1)
+            {
+                currentTexture = 0;
+            }
+
             Texture2D texture2D = (Texture2D)textures[currentTexture];
 
             return texture2D.GetPixel(0, texture2D.height);
