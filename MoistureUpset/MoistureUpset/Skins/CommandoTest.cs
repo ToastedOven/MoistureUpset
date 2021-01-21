5f2f0bad4853044c501b9f2955377db41e8212a2
Zachary Jones <dwelxs2@gmail.com>
2021-01-21 11:47:17 -0500

    puro added :crab:

--- MoistureUpset/MoistureUpset/Skins/CommandoTest.cs
+++ MoistureUpset/MoistureUpset/Skins/CommandoTest.cs
@@ -67,6 +67,13 @@ namespace MoistureUpset.Skins
 
                 RendererInfos = new CharacterModel.RendererInfo[]
                 {
+                    new CharacterModel.RendererInfo
+                    {
+                        defaultMaterial = Resources.Load<Material>("@MoistureUpset_moisture_puro:assets/puro/puro.mat"),
+                        defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                        ignoreOverlays = false,
+                        renderer = renderers[2]
+                    },
                 },
 
                 MeshReplacements = new SkinDef.MeshReplacement[]
@@ -81,6 +88,10 @@ namespace MoistureUpset.Skins
                 MinionSkinReplacements = new SkinDef.MinionSkinReplacement[0]
             };
 
+            skin.RendererInfos[0].defaultMaterial = GameObject.Instantiate(Resources.Load<GameObject>("prefabs/characterbodies/CommandoBody")).GetComponentInChildren<SkinnedMeshRenderer>().material;
+            skin.RendererInfos[0].defaultMaterial.mainTexture = Resources.Load<Material>("@MoistureUpset_moisture_puro:assets/puro/puro.mat").mainTexture;
+
+
             Array.Resize(ref skinController.skins, skinController.skins.Length + 1);
             skinController.skins[skinController.skins.Length - 1] = LoadoutAPI.CreateNewSkinDef(skin);
 
@@ -93,163 +104,28 @@ namespace MoistureUpset.Skins
             DebugClass.Log($"Adding skin: {Name}");
 
 
+            BoneAdder.boneList = bodyPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones;
+            BoneAdder.AddToBoneList("@MoistureUpset_moisture_puro:assets/puro/purobones.prefab", "purobones");
+            bodyPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones = BoneAdder.boneList;
+            BoneAdder.AppendAtoB("TailC", "pelvis");
+            BoneAdder.AppendAtoB("EarC.R", "head");
+            BoneAdder.AppendAtoB("EarC.L", "head");
 
-            List<Transform> t = new List<Transform>();
-            foreach (var item in bodyPrefab.GetComponentsInChildren<SkinnedMeshRenderer>())
-            {
-                foreach (var bone in item.bones)
-                {
-                    t.Add(bone);
-                }
-            }
-            var tail = Resources.Load<GameObject>("@MoistureUpset_moisture_puro:assets/puro/tailc.prefab");
-            foreach (var boner in tail.GetComponentsInChildren<Transform>())
-            {
-                if (boner.name.Contains("_end"))
-                {
-                    continue;
-                }
-                t.Add(boner);
-            }
-            var earL = Resources.Load<GameObject>("@MoistureUpset_moisture_puro:assets/puro/earc.l.prefab");
-            foreach (var boner in earL.GetComponentsInChildren<Transform>())
-            {
-                if (boner.name.Contains("_end"))
-                {
-                    continue;
-                }
-                t.Add(boner);
-            }
-            var earR = Resources.Load<GameObject>("@MoistureUpset_moisture_puro:assets/puro/earc.r.prefab");
-            foreach (var boner in earR.GetComponentsInChildren<Transform>())
-            {
-                if (boner.name.Contains("_end"))
-                {
-                    continue;
-                }
-                t.Add(boner);
-            }
-            bodyPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones = t.ToArray();
-
-            DynamicBone dynbone;
-            var i = 0;
-            Transform pelvis = bodyPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones[0];
-            Transform head = bodyPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones[0];
-            foreach (var item in bodyPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones)
-            {
-                if (item.name == "head")
-                {
-                    head = item;
-                }
-                if (item.name == "pelvis")
-                {
-                    pelvis = item;
-                }
-                if (item.name == "TailC")
-                {
-                    item.SetParent(pelvis);
-                    item.transform.position = pelvis.position;
-                    item.transform.rotation = Quaternion.identity;
-                    item.transform.localRotation = new Quaternion(-.7f, .4f, .3f, -.5f);
-                    item.transform.localPosition = new Vector3(.1f, .2f, 0);
-                    //bodyPrefab.AddComponent<RotationTest>().t = item;
-                    dynbone = bodyPrefab.AddComponent<DynamicBone>();
-                    dynbone.m_Root = item.GetChild(0);
-                }
-                if (item.name == "EarC.R")
-                {
-                    item.SetParent(head);
-                    item.transform.position = head.position;
-                    item.transform.rotation = Quaternion.identity;
-                    item.transform.localRotation = new Quaternion(-.1f, -.1f, .2f, 1);
-                    dynbone = bodyPrefab.AddComponent<DynamicBone>();
-                    dynbone.m_Root = item.GetChild(0);
-                }
-                if (item.name == "EarC.L")
-                {
-                    item.SetParent(head);
-                    item.transform.position = head.position;
-                    item.transform.rotation = Quaternion.identity;
-                    item.transform.localRotation = new Quaternion(-.1f, -.1f, -.2f, 1);
-                    dynbone = bodyPrefab.AddComponent<DynamicBone>();
-                    dynbone.m_Root = item.GetChild(0);
-                }
-                //Debug.Log($"{i}--------{item}-----parent:{item.parent}");
-                i++;
-            }
-        }
-    }
+            BoneAdder.boneList = survivorDef.displayPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones;
+            BoneAdder.AddToBoneList("@MoistureUpset_moisture_puro:assets/puro/purobonesdisplay.prefab", "purobonesdisplay");
+            survivorDef.displayPrefab.GetComponentInChildren<SkinnedMeshRenderer>().bones = BoneAdder.boneList;
+            BoneAdder.AppendAtoB("TailC", "pelvis");
+            BoneAdder.AppendAtoB("EarC.R", "head");
+            BoneAdder.AppendAtoB("EarC.L", "head");
 
 
-    public class RotationTest : MonoBehaviour
-    {
-        public Transform t;
-        void Update()
-        {
-            //transform.rotation is Identity 
-            //on Space.Self aka local rotation
-            //(0,0,1) == https://cdn.discordapp.com/attachments/483371638340059156/801122731763564564/unknown.png
-            //(0,1,0) == https://cdn.discordapp.com/attachments/483371638340059156/801123449736265788/unknown.png
-            //(1,0,0) == https://cdn.discordapp.com/attachments/483371638340059156/801123975492927528/unknown.png
-            if (Input.GetKey(KeyCode.U))
-            {
-                t.Rotate(new Vector3(10 * Time.deltaTime, 0, 0), Space.Self);
-            }
-            if (Input.GetKey(KeyCode.J))
-            {
-                t.Rotate(new Vector3(-10 * Time.deltaTime, 0, 0), Space.Self);
-            }
-            if (Input.GetKey(KeyCode.I))
-            {
-                t.Rotate(new Vector3(0, 10 * Time.deltaTime, 0), Space.Self);
-            }
-            if (Input.GetKey(KeyCode.K))
-            {
-                t.Rotate(new Vector3(0, -10 * Time.deltaTime, 0), Space.Self);
-            }
-            if (Input.GetKey(KeyCode.O))
-            {
-                t.Rotate(new Vector3(0, 0, 10 * Time.deltaTime), Space.Self);
-            }
-            if (Input.GetKey(KeyCode.L))
-            {
-                t.Rotate(new Vector3(0, 0, -10 * Time.deltaTime), Space.Self);
-            }
+            //need to add to child locator to make this work I think
+            //ItemDisplayRuleSet IDRS = bodyPrefab.GetComponentInChildren<CharacterModel>().itemDisplayRuleSet;
 
-            if (Input.GetKey(KeyCode.R))
-            {
-                t.Translate(new Vector3(0, 1 * Time.deltaTime, 0), Space.World);
-            }
-            if (Input.GetKey(KeyCode.Y))
-            {
-                t.Translate(new Vector3(0, -1 * Time.deltaTime, 0), Space.World);
-            }
-            if (Input.GetKey(KeyCode.F))
-            {
-                t.Translate(new Vector3(0, 0, 1 * Time.deltaTime), Space.World);
-            }
-            if (Input.GetKey(KeyCode.H))
-            {
-                t.Translate(new Vector3(0, 0, -1 * Time.deltaTime), Space.World);
-            }
-            if (Input.GetKey(KeyCode.T))
-            {
-                t.Translate(new Vector3(1 * Time.deltaTime, 0, 0), Space.World);
-            }
-            if (Input.GetKey(KeyCode.G))
-            {
-                t.Translate(new Vector3(-1 * Time.deltaTime, 0, 0), Space.World);
-            }
-
-
-            if (Input.GetKey(KeyCode.Z))
-            {
-                Debug.Log($"--------{t.localRotation}");
-            }
-            if (Input.GetKey(KeyCode.X))
-            {
-                Debug.Log($"--------{t.localPosition}");
-            }
+            //IDRS.FindItemDisplayRuleGroup("Mushroom").rules[0].localPos = new Vector3(0.009687734f, 0.07507756f, -0.1626426f);
+            //IDRS.FindItemDisplayRuleGroup("Mushroom").rules[0].localAngles = new Vector3(-148.959f, 0f, 180f);
+            //IDRS.FindItemDisplayRuleGroup("Mushroom").rules[0].localScale = new Vector3(0.04764923f, 0.0476492f, 0.04764921f);
+            //IDRS.FindItemDisplayRuleGroup("Mushroom").rules[0].childName = "Tail.001";
         }
     }
 }
