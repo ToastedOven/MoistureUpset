69c24d60534d9356d91e9b7ab1244305028b2b3c
ToastedOven <49164897+ToastedOven@users.noreply.github.com>
2022-03-12 16:00:47 -0500

    Revert "Merge branch 'master' into Release"
    This reverts commit 2a2ad0decd2369b1c1c55019d9d543f0261a9917, reversing
    changes made to ae962fdc42ea60f88f89f9a86412ab52951b64d3.

--- MoistureUpset/MoistureUpset/Collabs/PlayableScavenger.cs
+++ MoistureUpset/MoistureUpset/Collabs/PlayableScavenger.cs
@@ -13,31 +13,31 @@ using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Text;
 using RiskOfOptions;
-//using PallesenProductions;
+using PallesenProductions;
 
 namespace MoistureUpset.Collabs
 {
     class PlayableScavenger
     {
-        //public static void Run()
-        //{
-        //    PallesenProductions.PlayableScavenger s = new PallesenProductions.PlayableScavenger();
-        //    foreach (var item in SurvivorCatalog.allSurvivorDefs)
-        //    {
-        //        if (item.bodyPrefab.name == "ScavengerPlayer")
-        //        {
-        //            EnemyReplacements.ReplaceModel(item.bodyPrefab, "@MoistureUpset_scavenger:assets/bosses/Backpack.mesh", 0);
-        //            EnemyReplacements.ReplaceModel(item.bodyPrefab, "@MoistureUpset_na:assets/na1.mesh", 1);
-        //            EnemyReplacements.ReplaceModel(item.bodyPrefab, "@MoistureUpset_scavenger:assets/bosses/Body.mesh", 2);
-        //            EnemyReplacements.ReplaceMeshFilter(item.bodyPrefab, "@MoistureUpset_scavenger:assets/bosses/gun.mesh");
+        public static void Run()
+        {
+            PallesenProductions.PlayableScavenger s = new PallesenProductions.PlayableScavenger();
+            foreach (var item in SurvivorCatalog.allSurvivorDefs)
+            {
+                if (item.bodyPrefab.name == "ScavengerPlayer")
+                {
+                    EnemyReplacements.ReplaceModel(item.bodyPrefab, "@MoistureUpset_scavenger:assets/bosses/Backpack.mesh", 0);
+                    EnemyReplacements.ReplaceModel(item.bodyPrefab, "@MoistureUpset_na:assets/na1.mesh", 1);
+                    EnemyReplacements.ReplaceModel(item.bodyPrefab, "@MoistureUpset_scavenger:assets/bosses/Body.mesh", 2);
+                    EnemyReplacements.ReplaceMeshFilter(item.bodyPrefab, "@MoistureUpset_scavenger:assets/bosses/gun.mesh");
 
-        //            EnemyReplacements.ReplaceModel(item.displayPrefab, "@MoistureUpset_scavenger:assets/bosses/Backpack.mesh", 0);
-        //            EnemyReplacements.ReplaceModel(item.displayPrefab, "@MoistureUpset_na:assets/na1.mesh", 1);
-        //            EnemyReplacements.ReplaceModel(item.displayPrefab, "@MoistureUpset_scavenger:assets/bosses/Body.mesh", 2);
-        //            EnemyReplacements.ReplaceMeshFilter(item.displayPrefab, "@MoistureUpset_scavenger:assets/bosses/gun.mesh");
-        //        }
-        //        //LEMURIAN_NAME
-        //    }
-        //}
+                    EnemyReplacements.ReplaceModel(item.displayPrefab, "@MoistureUpset_scavenger:assets/bosses/Backpack.mesh", 0);
+                    EnemyReplacements.ReplaceModel(item.displayPrefab, "@MoistureUpset_na:assets/na1.mesh", 1);
+                    EnemyReplacements.ReplaceModel(item.displayPrefab, "@MoistureUpset_scavenger:assets/bosses/Body.mesh", 2);
+                    EnemyReplacements.ReplaceMeshFilter(item.displayPrefab, "@MoistureUpset_scavenger:assets/bosses/gun.mesh");
+                }
+                //LEMURIAN_NAME
+            }
+        }
     }
 }
